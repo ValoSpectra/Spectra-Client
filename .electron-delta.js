56e56b451dd57694ac665acc14c543c025dff302
Dark <7890309+DarkView@users.noreply.github.com>
2025-01-11 17:48:50 +0100

    Bump version

--- .electron-delta.js
+++ .electron-delta.js
@@ -3,7 +3,7 @@ const DeltaBuilder = require("@electron-delta/builder");
 const path = require("path");
 
 // Last 5 versions
-const previousVersions = ["0.2.8", "0.2.7", "0.2.6", "0.2.5"];
+const previousVersions = ["0.2.9", "0.2.8", "0.2.7", "0.2.6", "0.2.5"];
 
 const options = {
   productIconPath: path.join(__dirname, "/build/icon.ico"),
